fa28bfbaa37b50b089585b572f5634178c4d5758
Matilda <matilda@thewebco.co.nz>
2021-07-20 10:52:12 +1200

    Get, Post Put Delete added and tested via postman

--- src/main/java/com/vectortest/Test/Repo/UserRepo.java
+++ src/main/java/com/vectortest/Test/Repo/UserRepo.java
@@ -2,5 +2,6 @@ package com.vectortest.Test.Repo;
 import com.vectortest.Test.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo extends JpaRepository<User, Long> {
+public interface UserRepo extends JpaRepository<User, String> {
+    User findByEmail(String email);
 }
--- src/main/java/com/vectortest/Test/controller/VectorTestController.java
+++ src/main/java/com/vectortest/Test/controller/VectorTestController.java
@@ -1,9 +1,11 @@
 package com.vectortest.Test.controller;
 
 import com.vectortest.Test.Repo.UserRepo;
+import com.vectortest.Test.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class VectorTestController {
@@ -13,7 +15,33 @@ public class VectorTestController {
     public String getPage(){
         return "Default Page";
     }
+    @GetMapping(value = "/user")
+    public List<User> getUsers(){
+        return userRepo.findAll();
+    }
+    @PostMapping(value ="/user")
+    public String saveUser(@RequestBody User user){
+        userRepo.save(user);
+        return "The user was created successfully.";
+    }
 
+    @PutMapping(value = "user/{email}")
+    public String updateUser(@PathVariable String email, @RequestBody User user){
+        User updatedUser = userRepo.findByEmail(email);
+        updatedUser.setFirstname(user.getFirstname());
+        updatedUser.setLastname(user.getLastname());
+        updatedUser.setEmail(user.getEmail());
+        updatedUser.setPassword(user.getPassword());
+        userRepo.save(updatedUser);
+        return "The user was updated successfully.";
+    }
+
+    @DeleteMapping(value = "user/{email}")
+    public String deleteUser(@PathVariable String email){
+        User deleteUser =userRepo.findByEmail(email);
+        userRepo.delete(deleteUser);
+        return "The user was deleted successfully";
+    }
 
 
 }
