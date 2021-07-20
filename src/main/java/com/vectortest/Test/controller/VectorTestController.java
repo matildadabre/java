6b367ee86b39d288ba545067140153b6985d5015
Matilda <matilda@thewebco.co.nz>
2021-07-20 14:38:08 +1200

    Added comments to class

--- src/main/java/com/vectortest/Test/controller/VectorTestController.java
+++ src/main/java/com/vectortest/Test/controller/VectorTestController.java
@@ -11,20 +11,26 @@ import java.util.List;
 public class VectorTestController {
     @Autowired
     private UserRepo userRepo;
+
+    //Default page
     @GetMapping(value = "/")
     public String getPage(){
         return "Default Page";
     }
+    //Get all users
     @GetMapping(value = "/user")
     public List<User> getUsers(){
         return userRepo.findAll();
     }
+
+    //Save user
     @PostMapping(value ="/user")
     public String saveUser(@RequestBody User user){
         userRepo.save(user);
         return "The user was created successfully.";
     }
 
+    //Update User
     @PutMapping(value = "user/{email}")
     public String updateUser(@PathVariable String email, @RequestBody User user){
         User updatedUser = userRepo.findByEmail(email);
@@ -36,6 +42,7 @@ public class VectorTestController {
         return "The user was updated successfully.";
     }
 
+    //Delete user
     @DeleteMapping(value = "user/{email}")
     public String deleteUser(@PathVariable String email){
         User deleteUser =userRepo.findByEmail(email);
